d98c4fc20fb2bb3d9f71f517fbb132c9e422a9e3
Christian Dell <christian.dell1@gmail.com>
2023-07-01 01:25:22 -0700

    giving up on RGB keys and colemak for now
    I'm wired up for I2C
    using EE_HANDS, so qmk flash -bl avrdude-split-[right/left] when flashing to either half
    maybe I should have left these config changes in the keymap's config?
    TODO: kill off the layers for now? rethink keymaps, especially thumb clusters

--- keyboards/handwired/dactyl_manuform/6x6/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/config.h
@@ -1,5 +1,6 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,11 +16,25 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #pragma once
 
-#define MASTER_LEFT
-//#define MASTER_RIGHT
-//#define EE_HANDS
-// Rows are doubled-up
 
+#define USE_I2C
+
+#define SPLIT_LAYER_STATE_ENABLE
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 14
+#define MATRIX_COLS 6
+
+// wiring of each half
+// I reversed the columns when wiring the left side
+//#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4 }
+#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
+
+// switched from COL2ROW for my amoeba PCBs
+#define DIODE_DIRECTION ROW2COL
+
+#define EE_HANDS
--- keyboards/handwired/dactyl_manuform/6x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/6x6/rules.mk
@@ -1,1 +1,22 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+MIDI_ENABLE = no
+AUDIO_ENABLE = no           # Audio output
+UNICODE_ENABLE = no
+BLUETOOTH_ENABLE = no
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
+SPLIT_KEYBOARD = yes
+
+SLEEP_LED_ENABLE = no
+
 DEFAULT_FOLDER = handwired/dactyl_manuform/6x6/promicro
